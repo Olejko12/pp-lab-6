67b1168e63bd8ee9054ead867588f551a4b7726c
Olejko12 <162018682+Olejko12@users.noreply.github.com>
2024-05-05 13:13:46 +0200

    zad4

--- Main.java
+++ Main.java
@@ -13,7 +13,9 @@ public class Main {
         Worker worker1 = new Worker("Krzysiu", 1, 5000, "14.01.2022", "Janitor");
         Worker worker2 = new Worker("Areczek", 2, 4500, "22.02.2021", "assistant");
         Worker worker3 = new Worker("Tomeczek", 2, 4300, "08.03.2023", "developer");
+        Worker worker4 = new Worker("Michał",2,3800,"17.04.2024","Janitor");
         Manager manager = new Manager("Konrad", 7, 7500, "16.12.2020", "Manager");
+        Manager manager2 = new Manager("Marek", 8,7000,"27.09.2021","manager");
 
         employees.add(worker1);
         employees.add(worker2);
@@ -31,5 +33,11 @@ public class Main {
         System.out.println("worker3 "+worker3.getName()+ " equals worker1 "+worker1.getName()+": " + worker3.equals(worker1));
         System.out.println("worker3 "+worker3.getName()+ " quals worker2 "+worker2.getName()+": " + worker3.equals(worker2));
         System.out.println("worker3 "+worker3.getName()+ " equals manager "+manager.getName()+": " + worker3.equals(manager));
+
+        double totalSalary = 0;
+        for (Employee employee : employees){
+            totalSalary += employee.getSalary();
+        }
+        System.out.println("Suma w");
     }
 }
